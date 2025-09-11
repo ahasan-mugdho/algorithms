205ce2c02775f8ac631f4bda3dcba6b2d1f98f55
ahasan mugdho <ahasan.mugdho.cse10.bu@gmail.com>
2025-09-11 20:47:14 +0600

    Create bubbleSort.cpp

---
+++ bubbleSort.cpp
@@ -0,0 +1,16 @@
+void bubble_sort(int array[],int size)
+{
+    int i,j,temp;
+    for(i=0;i<size-1;i++)
+    {
+        for(j=0;j<size-i-1;j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+}
